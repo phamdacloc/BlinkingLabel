295ad6f1d02a46dd564b96384209c147c32f5840
Pham Dac Loc <pham.loc@gmail.com>
2015-12-19 18:50:35 +0700

    Another commit hopefully will pickup all the changes.

--- Pod/Classes/BlinkingLabel.swift
+++ Pod/Classes/BlinkingLabel.swift
@@ -1,9 +1,13 @@
-//
-//  BlinkingLabel.swift
-//  Pods
-//
-//  Created by Pham Dac Loc on 12/19/15.
-//
-//
-
-import Foundation
+public class BlinkingLabel : UILabel {
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = [.Repeat, .Autoreverse]
+        UIView.animateWithDuration(0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+            }, completion: nil)
+    }
+    
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
